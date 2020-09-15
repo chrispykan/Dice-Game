9241b0b8ae22d9e841e6e9d1adbce74cd4b23ad8
chrispykan <christineparisk@gmail.com>
2020-09-16 01:21:26 +0200

    added objectives, defined Dice class

--- Dice.java
+++ Dice.java
@@ -1,4 +1,29 @@
+/* -Write a class, Dice, which defines a dice object
+   -A constructor that initializes the die to 0
+   -A Getter that gets the value of the dice
+   -A Method that rolls the dice*/
+   
 import java.util.*;
 
 public class Dice{
+   int value;
+   //using the Random class from java.util to generate a random number when rolling dice 
+   Random r = new Random(); 
+   
+   //Constructor
+   public Dice(){
+      this.value = value;
+   }
+   
+   //Getter
+   public int getValue(){
+      return value;
+   }
+   
+   //Roll Method
+   public void roll(){
+   //dice has 6 sides, +1 to avoid generating a 0.
+   value = r.nextInt(6)+1;
+   }
+
 }
--- Game.java
+++ Game.java
@@ -1,3 +1,7 @@
+/* -Game consists of playing 10 games
+   -Each game has 3 rounds
+   -Player wins the round by rolling a 6*/
+
 public class Game{
    public static void main (String[] args){
    
